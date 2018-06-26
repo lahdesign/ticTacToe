1d68ee257b9ae7a8e642f55f136b7c72b1c9fd06
lahdesign <lahdesign@gmail.com>
2018-06-26 13:13:48 -0400

    code is more object oriented & three new images

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -10,19 +10,23 @@ const game = require('./ticTacToe/game')
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
-$(() => {
-  // let PlayerXTurn = true
-  // const gameBoard = []
+//TODO make an instance of game using constructor function
 
-  startGame = function() {
+$(() => {
+
+  const Game = function() {
       this.count = 0
       let PlayerXTurn = true
-      const gameBoard = []
-      gameOn = true
+      this.gameBoard = []
+      this.gameOn = true
+  }
+  Game.prototype.startGame = function() {
+    this.gameOn = true
+  }
+  // const game = new Game()
+  // gives game all of the starter principles
 
-  endGame = function() {
-       this.count = 9
-       gameOn = false
+  game.startGame()
 
   // claim spot function
   $('.gameButtons').click(function (event) {
@@ -30,34 +34,40 @@ $(() => {
     if (PlayerXTurn) {
       gameBoard[event.target.id] = 'X'
       $(this).addClass('ximage')
+      game.count++
     } else {
       gameBoard[event.target.id] = 'O'
       $(this).addClass('oimage')
     }
     PlayerXTurn = !PlayerXTurn
+    count++
     console.log(gameBoard)
     alert(event)
 
-    // TODO add a game counter here. I can use a count++ here
+    // Is that the correct place to do count++ here
   })
 }
   // TODO - how do these things go somewhere other than the console log? Get saved in an array that can get fed to
   // the check if winner function, check if tie
   // function to check if winner
-//   const checkIfWinner = function () {
-//     if ((this.gameBoard[0] === this.gameBoard[1] && this.gameBoard[1] === this.gameBoard[2]) ||
-//         (this.gameBoard[3] === this.gameBoard[4] && this.gameBoard[4] === this.gameBoard[5]) ||
-//         (this.gameBoard[6] === this.gameBoard[7] && this.gameBoard[7] === this.gameBoard[8]) ||
-//         (this.gameBoard[0] === this.gameBoard[3] && this.gameBoard[3] === this.gameBoard[6]) ||
-//         (this.gameBoard[1] === this.gameBoard[4] && this.gameBoard[4] === this.gameBoard[7]) ||
-//         (this.gameBoard[2] === this.gameBoard[5] && this.gameBoard[5] === this.gameBoard[8]) ||
-//         (this.gameBoard[0] === this.gameBoard[4] && this.gameBoard[4] === this.gameBoard[8]) ||
-//         (this.gameBoard[2] === this.gameBoard[4] && this.gameBoard[4] === this.gameBoard[6])) {
 
-//       if (playerXTurn) {
-//         alert("player_x");
-//       } else {
-//         alert("player_o");
+  const checkIfWinner = function (gameBoard) {
+    if ((this.gameBoard[0] === this.gameBoard[1] && this.gameBoard[1] === this.gameBoard[2]) ||
+        (this.gameBoard[3] === this.gameBoard[4] && this.gameBoard[4] === this.gameBoard[5]) ||
+        (this.gameBoard[6] === this.gameBoard[7] && this.gameBoard[7] === this.gameBoard[8]) ||
+        (this.gameBoard[0] === this.gameBoard[3] && this.gameBoard[3] === this.gameBoard[6]) ||
+        (this.gameBoard[1] === this.gameBoard[4] && this.gameBoard[4] === this.gameBoard[7]) ||
+        (this.gameBoard[2] === this.gameBoard[5] && this.gameBoard[5] === this.gameBoard[8]) ||
+        (this.gameBoard[0] === this.gameBoard[4] && this.gameBoard[4] === this.gameBoard[8]) ||
+        (this.gameBoard[2] === this.gameBoard[4] && this.gameBoard[4] === this.gameBoard[6])) {
+
+      if (playerXTurn) {
+        console.log("player_x");
+          $(this).addClass('tieimage')
+
+      } else {
+        console.log("player_o");
+          $(this).addClass('tieimage')
 //       }
 //
 //     }
@@ -65,9 +75,6 @@ $(() => {
 // })
 /
 // }
-
-// (function() {
-// 	let game = {
 
 
 //   gameBoardState = [null, 0,1,2,3,4,5,6,7,8],
@@ -77,15 +84,21 @@ $(() => {
 // },
 
 // checkIfAvailable: function() {
-// checkIfWinner: function() {
+
 // checkIfGameInSession: function() {
 
-checkIfTie: function() {
+checkTie = function() {
   if (this.count === 9) {
     return true;
+    $(this).addClass('tieimage')
   } else {
     return false;
   };
 }
+//TODO rewrite to work with constructors
+endGame = function() {
+  this.count = 9
+  gameOn = false
 
-/ module.exports = {
+
+ module.exports = {
