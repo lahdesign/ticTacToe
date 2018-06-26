c18c7486c892d2b6de24df2c7125ed7d7e266455
lahdesign <lahdesign@gmail.com>
2018-06-26 16:45:13 -0400

    corrected claim, got board to display

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,7 +2,8 @@
 const getFormFields = require('../../lib/get-form-fields')
 const api = require('./ticTacToe/api')
 const ui = require('./ticTacToe/ui')
-const game = require('./ticTacToe/game')
+const Game = require('./ticTacToe/game')
+const gameInstance = new Game()
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
@@ -10,66 +11,5 @@ const game = require('./ticTacToe/game')
 // use require without a reference to ensure a file is bundled
 // require('./example')
 $(() => {
-  const Game = function () {
-    this.count = 0
-    this.gameOn = true
-    this.playerXTurn = true
-    this.gameBoard = []
-  }
-  Game.prototype.playGame = function () {
-    $('.gameButtons').click(function (event, playerXTurn, gameBoard, count) {
-      event.preventDefault()
-      if (playerXTurn) {
-        gameBoard[event.target.id] = 'X'
-        $(this).addClass('xImage')
-        this.game.count++
-      } else {
-        gameBoard[event.target.id] = 'O'
-        $(this).addClass('oImage')
-        this.game.count++
-      }
-      playerXTurn = !playerXTurn
-      count++
-      console.log(gameBoard)
-      alert(event)
-    })
-  }
-  Game.prototype.checkIfWinner = function (gameBoard, playerXTurn) {
-    if ((this.gameBoard[0] === this.gameBoard[1] && this.gameBoard[1] === this.gameBoard[2]) ||
-        (this.gameBoard[3] === this.gameBoard[4] && this.gameBoard[4] === this.gameBoard[5]) ||
-        (this.gameBoard[6] === this.gameBoard[7] && this.gameBoard[7] === this.gameBoard[8]) ||
-        (this.gameBoard[0] === this.gameBoard[3] && this.gameBoard[3] === this.gameBoard[6]) ||
-        (this.gameBoard[1] === this.gameBoard[4] && this.gameBoard[4] === this.gameBoard[7]) ||
-        (this.gameBoard[2] === this.gameBoard[5] && this.gameBoard[5] === this.gameBoard[8]) ||
-        (this.gameBoard[0] === this.gameBoard[4] && this.gameBoard[4] === this.gameBoard[8]) ||
-        (this.gameBoard[2] === this.gameBoard[4] && this.gameBoard[4] === this.gameBoard[6])) {
-      if (playerXTurn) {
-        console.log('player_x wins')
-        $(this).addClass('xWinsImage')
-      } else {
-        console.log('player_o wins')
-        $(this).addClass('oWinsImage')
-      }
-    }
-  }
-  Game.prototype.checkTie = function () {
-    if (this.count === 9) {
-      $(this).addClass('tieImage')
-      return true
-    } else {
-      return false
-    }
-  }
-  Game.prototype.endGame = function () {
-    this.count = 9
-    this.gameOn = false
-  }
+  gameInstance.playGame()
 })
-
-Game()
-
- // Game.prototype.checkIfAvailable = function () {
-  // }
-
-  // Game.prototype.checkIfGameInSession = function () {
-  // }
---
+++ assets/scripts/ticTacToe/game.js
@@ -0,0 +1,58 @@
+class Game {
+  constructor () {
+    this.count = 0
+    this.gameOn = true
+    this.playerXTurn = true
+    this.gameBoard = []
+  }
+  claim (event) {
+    event.preventDefault()
+    if (this.playerXTurn) {
+      this.gameBoard[event.target.id] = 'X'
+      $(event.target).addClass('xImage')
+      this.count++
+    } else {
+      this.gameBoard[event.target.id] = 'O'
+      $(event.target).addClass('oImage')
+      this.count++
+    }
+    this.playerXTurn = !this.playerXTurn
+    this.count++
+    this.checkIfWinner()
+  }
+  playGame () {
+    const bound = this.claim.bind(this)
+    $('.gameButtons').click(bound)
+  }
+  checkIfWinner (playerXTurn) {
+    if ((this.gameBoard[0] === this.gameBoard[1] && this.gameBoard[1] === this.gameBoard[2]) ||
+      (this.gameBoard[3] === this.gameBoard[4] && this.gameBoard[4] === this.gameBoard[5]) ||
+      (this.gameBoard[6] === this.gameBoard[7] && this.gameBoard[7] === this.gameBoard[8]) ||
+      (this.gameBoard[0] === this.gameBoard[3] && this.gameBoard[3] === this.gameBoard[6]) ||
+      (this.gameBoard[1] === this.gameBoard[4] && this.gameBoard[4] === this.gameBoard[7]) ||
+      (this.gameBoard[2] === this.gameBoard[5] && this.gameBoard[5] === this.gameBoard[8]) ||
+      (this.gameBoard[0] === this.gameBoard[4] && this.gameBoard[4] === this.gameBoard[8]) ||
+      (this.gameBoard[2] === this.gameBoard[4] && this.gameBoard[4] === this.gameBoard[6])) {
+      if (playerXTurn) {
+        console.log('player_x wins')
+        $(this).addClass('xWinsImage')
+      } else {
+        console.log('player_o wins')
+        $(this).addClass('oWinsImage')
+      }
+    }
+  }
+  checkTie () {
+    if (this.count === 9) {
+      $(this).addClass('tieImage')
+      return true
+    } else {
+      return false
+    }
+  }
+  endGame () {
+    this.count = 9
+    this.gameOn = false
+  }
+}
+module.exports = Game
